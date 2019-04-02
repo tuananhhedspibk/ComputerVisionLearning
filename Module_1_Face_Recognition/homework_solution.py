5fc9cab59f2d76f4581ab01976f286d7b2bb9bc8
tt-anh-eole <tt-anh@eole.co.jp>
2019-04-02 10:03:34 +0900

    Update for smile dectection

--- Module_1_Face_Recognition/homework_solution.py
+++ Module_1_Face_Recognition/homework_solution.py
@@ -5,6 +5,7 @@ import cv2
 # Loading the cascades
 face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('./haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('./haarcascade_smile.xml')
 
 # Defining a function that will do the detection
 def detect(gray, frame):
@@ -13,9 +14,13 @@ def detect(gray, frame):
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
     return frame
 
 video_capture = cv2.VideoCapture(0)
