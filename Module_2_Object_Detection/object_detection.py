855b099371294af189842d958225b76ff52df5a1
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2019-04-02 23:25:42 +0900

    Finish Object Detection

--- Module_2_Object_Detection/object_detection.py
+++ Module_2_Object_Detection/object_detection.py
@@ -21,11 +21,26 @@ def detect(frame, net, transform):
         j = 0
         while detections[0, i, j, 0] >= 0.6:
             pt = (detections[0, i, j, 1:] * scale).numpy()
-            cv2.rectangle(frame, (int(pt[0]), int(pt[1])), (int(pt[2]), int(pt[3])), (255, 0, 0), 2)
-            cv2.putText(frame, labelmap[i - 1], (int(pt[0]), int(pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.rectangle(frame, (int(pt[0]), int(pt[1])),
+                                  (int(pt[2]), int(pt[3])), (255, 0, 0), 2)
+            cv2.putText(frame, labelmap[i - 1], (int(pt[0]),
+                        int(pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 2,
+                        (255, 255, 255), 2, cv2.LINE_AA)
             j += 1
     return frame
 
 net = build_ssd("test")
-net.load_state_dict(torch.load("ssd300_mAP_77.43_v2.pth"), map_location = lambda storage, loc: storage)
+net.load_state_dict(torch.load("ssd300_mAP_77.43_v2.pth",
+                    map_location = lambda storage, loc: storage))
 
+transform = BaseTransform(net.size, (104 / 256.0, 117 / 256.0, 123 / 256.0))
+
+reader = imageio.get_reader("./funny_dog.mp4")
+fps = reader.get_meta_data()["fps"]
+writer = imageio.get_writer("./output.mp4", fps = fps)
+
+for i, frame in enumerate(reader):
+    frame = detect(frame, net.eval(), transform)
+    writer.append_data(frame)
+    print("Frame (" + str(i) + ")")
+writer.close()
